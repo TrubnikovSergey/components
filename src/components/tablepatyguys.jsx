2626f5c8a3c7793bb5440d8df56cfedd50c140e1
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2022-07-07 21:02:10 +0300

    refactorig code

--- src/components/tablepatyguys.jsx
+++ src/components/tablepatyguys.jsx
@@ -5,13 +5,11 @@ const TablePatyGuys = () => {
 
     const [guys, setGuys] = useState(API.users.fetchAll())
 
-    const qualitiesList = (qList, id) => {
+    const decorateQualities = (qualities) => {
 
-        const list = qList.map((item, i) => {
-    
+        const list = qualities.map(item => {    
             const classes = `m-2 badge bg-${item.color}`    
-            return <span key = {id + i} className = {classes}>{item.name}</span>
-
+            return <span key = {item._id} className = {classes}>{item.name}</span>
         })
     
         return list
@@ -21,76 +19,90 @@ const TablePatyGuys = () => {
         setGuys((oldState) => oldState.filter(guy => guy._id !== id))
     }
 
-    const users = guys
-                    .map(user => {
+    const users = guys.map(user => {
+        
+        const {_id, name, qualities, profession, completedMeetings, rate} = user
+        return ( 
+            <tr key = {_id}>
+                <td>{name}</td>
+                <td>{decorateQualities(qualities)}</td>
+                <td>{profession.name}</td>
+                <td>{completedMeetings}</td>
+                <td>{rate}/5</td>
+                <td><button 
+                        type = "button" 
+                        className = "btn btn-danger badge m-2" 
+                        onClick={() => handlerDeletGuy(user._id)}>delete
+                    </button>
+                </td>
+            </tr>
+        )
 
-                        return  <tr key = {user._id}>
-                                    <td>{user.name}</td>
-                                    <td>{qualitiesList(user.qualities, user._id)}</td>
-                                    <td>{user.profession.name}</td>
-                                    <td>{user.completedMeetings}</td>
-                                    <td>{user.rate}/5</td>
-                                    <td><button type = "button" className = "btn btn-danger badge m-2" onClick={() => handlerDeletGuy(user._id)}>delete</button></td>
-                                </tr>
+    })
 
-                    })
-
-    const thead =   <thead>
-                        <tr>
-                            <th scope = "col">Имя</th>
-                            <th scope = "col">Качество</th>
-                            <th scope = "col">Профессия</th>
-                            <th scope = "col">Встретился, раз</th>
-                            <th scope = "col">Оценка</th>
-                            <th scope = "col"></th>
-                        </tr>
-                    </thead>
+    const thead = (
+        <thead>
+            <tr>
+                <th scope = "col">Имя</th>
+                <th scope = "col">Качество</th>
+                <th scope = "col">Профессия</th>
+                <th scope = "col">Встретился, раз</th>
+                <th scope = "col">Оценка</th>
+                <th scope = "col"></th>
+            </tr>
+        </thead>)
 
     
-    const table =   <table className = "table">
-                            {thead}
-                            <tbody>
-                                {users}
-                            </tbody>
-                        </table>
+    const table = (  
+        <table className = "table">
+            {thead}
+            <tbody>
+                {users}
+            </tbody>
+        </table>)
 
     const stringCountGuys = (count) => {
 
-        const strCount = String(count)
-        const lastNumber = Number(strCount[strCount.length - 1])
+        if(count === 0){
+            return <span className = "badge bg-danger">Никто с тобой не тусанет</span>
+        }        
 
-        if(count === 1){
+        const lastNumber = (count % 10)
+        let strCount = ''
 
-            return `${count} человек тусанет с тобой сегодня`
+        if(count >= 5 && count <= 20){
+
+            strCount = `${count} человек тусанет`
+
+        }else if(lastNumber === 1 || lastNumber === 0){
+
+            strCount = `${count} человек тусанет`
+
+        }else if(lastNumber === 2 || lastNumber === 3 || lastNumber === 4){
+
+            strCount = `${count} человека тусанут`
+        }else{
+
+            strCount = `${count} человек тусанет`
         }
 
-        if(lastNumber === 1){
-
-            return `${count} человек тусанут с тобой сегодня`
-        }
-
-        if(count >= 5 && count <= 20) {
-
-            return `${count} человек тусанут с тобой сегодня`
-        }
-
-        if(lastNumber === 2 || lastNumber === 3 || lastNumber === 4){
-
-            return `${count} человека тусанут с тобой сегодня`
-        }
+        let stringcountguys = <span className = "badge bg-primary">{strCount} с тобой сегодня</span>
+        return stringcountguys
 
     }
 
     if(guys.length){
 
-        return  <>
-                    <span className = "badge bg-primary">{stringCountGuys(guys.length)}</span>
-                    {table}
-                </>
+        return  (
+            <>
+                {stringCountGuys(guys.length)}
+                {table}
+            </>
+        )
 
     }else{
 
-        return <span className = "badge bg-danger">Никто не хочет тусить</span>
+        return stringCountGuys(guys.length)
 
     }
 
